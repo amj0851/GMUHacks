a500f11e70361d54d7982aee3f70f7d615769b4d
Anna <amj0851@gmail.com>
2021-04-17 22:25:58 -0400

    project

---
+++ app/src/main/java/com/example/pollutionsimulator/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.pollutionsimulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        ProgressBar barOne = findViewById(R.id.progressBar5);
+        barOne.setMax(100);
+        //int amountOne = 10;
+
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            int amountOne = 10;
+            @Override
+            public void onClick(View v) {
+                amountOne = amountOne + 10;
+                barOne.setProgress(amountOne);
+            }
+        });
+    }
+
+
+}
